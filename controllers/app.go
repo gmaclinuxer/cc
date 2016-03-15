a24f53b9c22f7f4e630efb027c208263a52b4f57
shwinpiocess <springwind_lee@163.com>
2016-03-16 00:14:57 +0800

    修改代码逻辑

--- controllers/app.go
+++ controllers/app.go
@@ -2,72 +2,74 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/shwinpiocess/cc/models"
 	"strings"
-    "github.com/shwinpiocess/cc/models"
+	"time"
 )
 
 type AppController struct {
-    BaseController
+	BaseController
 }
 
 func (this *AppController) Index() {
-    num, apps, err := models.GetApps(this.userId)
-    if err == nil {
-        // 无任何业务时
-        if num == 0 {
-            this.TplName = "app/index_empty.html"
-        } else {
-            this.Data["apps"] = apps
-            this.Data["defApp"] = apps[0]
-            this.TplName = "app/index.html"
-        }
-    }
+	this.Data["today"] = time.Now().Format("20060102")
+	num, apps, err := models.GetApps(this.userId)
+	if err == nil {
+		// 无任何业务时
+		if num == 0 {
+			this.TplName = "app/index_empty.html"
+		} else {
+			this.Data["apps"] = apps
+			this.Data["defApp"] = apps[0]
+			this.TplName = "app/index.html"
+		}
+	}
 }
 
 func (this *AppController) NewApp() {
-    this.TplName = "app/newapp.html"
+	this.TplName = "app/newapp.html"
 }
 
 func (this *AppController) AddApp() {
-    if this.isPost() {
-        app := new(models.App)
-        app.Type, _ = this.GetInt("Type")
-        app.Level, _ = this.GetInt("Level")
-        app.ApplicationName = strings.TrimSpace(this.GetString("ApplicationName"))
-        app.LifeCycle = strings.TrimSpace(this.GetString("LifeCycle"))
-        app.OwnerId = this.userId
-        out := make(map[string]interface{})
-        if models.IsAppExistByName(app.ApplicationName) {
-            out["errInfo"] = "同名的业务已经存在！"
-            out["success"] = false
-            out["errCode"] = "0006"
-            this.jsonResult(out)
-        } 
-        if _, err := models.AddApp(app); err != nil {
-            out["errInfo"] = err.Error()
-            out["success"] = false
-            out["errCode"] = "0007"
-            this.jsonResult(out)
-        }
-        fmt.Println("uuuuuuuuuuuuuuuuuu")
-        cnt, err := models.GetAppCountByUserId(this.userId)
-        fmt.Println("cnt=", cnt, "err=", err)
-        if err == nil {
-            if cnt > 1 {
-                out["success"] = true
-                out["gotopo"] = 0
-                this.jsonResult(out)
-            }
-            
-            out["success"] = true
-            out["gotopo"] = 1
-            this.jsonResult(out)
-        }
-        out["success"] = false
-        out["errInfo"] = err.Error()
-        out["errCode"] = "0008"
-        this.jsonResult(out)
-    }
+	if this.isPost() {
+		app := new(models.App)
+		app.Type, _ = this.GetInt("Type")
+		app.Level, _ = this.GetInt("Level")
+		app.ApplicationName = strings.TrimSpace(this.GetString("ApplicationName"))
+		app.LifeCycle = strings.TrimSpace(this.GetString("LifeCycle"))
+		app.OwnerId = this.userId
+		out := make(map[string]interface{})
+		if models.IsAppExistByName(app.ApplicationName) {
+			out["errInfo"] = "同名的业务已经存在！"
+			out["success"] = false
+			out["errCode"] = "0006"
+			this.jsonResult(out)
+		}
+		if _, err := models.AddApp(app); err != nil {
+			out["errInfo"] = err.Error()
+			out["success"] = false
+			out["errCode"] = "0007"
+			this.jsonResult(out)
+		}
+		fmt.Println("uuuuuuuuuuuuuuuuuu")
+		cnt, err := models.GetAppCountByUserId(this.userId)
+		fmt.Println("cnt=", cnt, "err=", err)
+		if err == nil {
+			if cnt > 1 {
+				out["success"] = true
+				out["gotopo"] = 0
+				this.jsonResult(out)
+			}
+
+			out["success"] = true
+			out["gotopo"] = 1
+			this.jsonResult(out)
+		}
+		out["success"] = false
+		out["errInfo"] = err.Error()
+		out["errCode"] = "0008"
+		this.jsonResult(out)
+	}
 }
 
 func (this *AppController) DeleteApp() {
@@ -81,4 +83,8 @@ func (this *AppController) DeleteApp() {
 		out["errInfo"] = err.Error()
 		this.jsonResult(out)
 	}
+}
+
+func (this *AppController) TopologyIndex() {
+	this.TplName = "topology/index.html"
 }
--- controllers/main.go
+++ controllers/main.go
@@ -2,60 +2,69 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/astaxie/beego"
 	"strconv"
-	"time"
 	"strings"
-    "github.com/astaxie/beego"
-    
-    "github.com/shwinpiocess/cc/models"
-    "github.com/shwinpiocess/cc/utils"
+	"time"
+
+	"github.com/shwinpiocess/cc/models"
+	"github.com/shwinpiocess/cc/utils"
 )
 
 type MainController struct {
-    BaseController
+	BaseController
 }
 
 func (this *MainController) Index() {
-    this.TplName = "index.html"
+	this.Data["today"] = time.Now().Format("20060102")
+	num, apps, err := models.GetApps(this.userId)
+	fmt.Println(apps)
+	if err == nil {
+		if num > 0{
+			this.TplName = "index.html"
+		} else {
+			this.redirect("/app/index")
+		}
+	}
 }
 
 func (this *MainController) Login() {
-    if this.userId > 0 {
-        this.redirect("/")
-    }
-    
-    beego.ReadFromRequest(&this.Controller)
-    if this.isPost() {
-        username := strings.TrimSpace(this.GetString("username"))
-        password := strings.TrimSpace(this.GetString("password"))
-        remember := this.GetString("remember")
-        if username != "" && password != "" {
-            user, err := models.GetUserByName(username)
-            errMsg := ""
-            if err != nil || user.Password != utils.Md5([]byte(password+user.Salt)) {
-                errMsg = "账号或密码错误"
-            } else if user.Status == -1 {
-                errMsg = "该账号已禁用"
-            } else {
-                user.LastIp = this.getClientIP()
-                user.LastLogin = time.Now().Unix()
-                models.UpdateUser(user)
-                
-                authkey := utils.Md5([]byte(this.getClientIP() + "|" + user.Password + user.Salt))
-                
-                if remember == "yes" {
-                    this.Ctx.SetCookie("auth", strconv.Itoa(user.Id) +"|" + authkey, 7 * 86400)
-                } else {
-                    this.Ctx.SetCookie("auth", strconv.Itoa(user.Id) +"|" + authkey)
-                }
-                this.redirect(beego.URLFor("AppController.Index"))
-            }
-            fmt.Println(errMsg)
-        }
-        
-    }
-    
-    this.TplName = "main/login.html"
+	if this.userId > 0 {
+		this.redirect("/")
+	}
+
+	beego.ReadFromRequest(&this.Controller)
+	if this.isPost() {
+		username := strings.TrimSpace(this.GetString("username"))
+		password := strings.TrimSpace(this.GetString("password"))
+		remember := this.GetString("remember")
+		if username != "" && password != "" {
+			user, err := models.GetUserByName(username)
+			errMsg := ""
+			if err != nil || user.Password != utils.Md5([]byte(password+user.Salt)) {
+				errMsg = "账号或密码错误"
+			} else if user.Status == -1 {
+				errMsg = "该账号已禁用"
+			} else {
+				user.LastIp = this.getClientIP()
+				user.LastLogin = time.Now().Unix()
+				models.UpdateUser(user)
+
+				authkey := utils.Md5([]byte(this.getClientIP() + "|" + user.Password + user.Salt))
+
+				if remember == "yes" {
+					this.Ctx.SetCookie("auth", strconv.Itoa(user.Id)+"|"+authkey, 7*86400)
+				} else {
+					this.Ctx.SetCookie("auth", strconv.Itoa(user.Id)+"|"+authkey)
+				}
+				this.redirect(beego.URLFor("AppController.Index"))
+			}
+			fmt.Println(errMsg)
+		}
+
+	}
+
+	this.TplName = "main/login.html"
 }
 
 // 退出登录
