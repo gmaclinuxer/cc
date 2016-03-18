dd7a6ab59b2995ed14368a39b9dfd03e8d37b05b
shwinpiocess <springwind_lee@163.com>
2016-03-19 02:06:46 +0800

    更新模型代码

--- models/app.go
+++ models/app.go
@@ -1,18 +1,21 @@
 package models
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
+	"strings"
 
 	"github.com/astaxie/beego/orm"
 )
 
 type App struct {
-	Id int
-	Type int
-	ApplicationName string
-	LifeCycle string
-	Level int
-	OwnerId int
+	Id              int    `orm:"column(id);auto"`
+	Type            int8   `orm:"column(type)"`
+	ApplicationName string `orm:"column(application_name);size(255)"`
+	LifeCycle       string `orm:"column(life_cycle);size(255)"`
+	Level           int8   `orm:"column(level)"`
+	OwnerId         int   `orm:"column(owner_id)"`
 }
 
 func (t *App) TableName() string {
@@ -23,13 +26,16 @@ func init() {
 	orm.RegisterModel(new(App))
 }
 
+// AddApp insert a new App into database and returns
+// last inserted Id on success.
 func AddApp(m *App) (id int64, err error) {
-	fmt.Println("999999999999999999=", m)
 	o := orm.NewOrm()
 	id, err = o.Insert(m)
 	return
 }
 
+// GetAppById retrieves App by Id. Returns error if
+// Id doesn't exist
 func GetAppById(id int) (v *App, err error) {
 	o := orm.NewOrm()
 	v = &App{Id: id}
@@ -39,35 +45,105 @@ func GetAppById(id int) (v *App, err error) {
 	return nil, err
 }
 
-func IsAppExistByName(applicationName string) (isExist bool) {
+// GetAllApp retrieves all App matches certain condition. Returns empty list if
+// no records exist
+func GetAllApp(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	return o.QueryTable("app").Filter("application_name", applicationName).Exist()
+	qs := o.QueryTable(new(App))
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		qs = qs.Filter(k, v)
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	var l []App
+	qs = qs.OrderBy(sortFields...)
+	if _, err := qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
 }
 
-func GetAppCountByUserId(userId int) (cnt int64, err error) {
+// UpdateApp updates App by Id and returns error if
+// the record to be updated doesn't exist
+func UpdateAppById(m *App) (err error) {
 	o := orm.NewOrm()
-	cnt, err = o.QueryTable("app").Filter("owner_id", userId).Count()
+	v := App{Id: m.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
 	return
 }
 
-func GetApps(userId int) (num int64, l []App, err error) {
-	o := orm.NewOrm()
-
-	if num, err = o.QueryTable(new(App)).Filter("owner_id", userId).OrderBy("id").All(&l); err == nil {
-		fmt.Println("num=", num)
-		return num, l, nil
-	}
-
-	return 0, nil, err
-}
-
+// DeleteApp deletes App by Id and returns error if
+// the record to be deleted doesn't exist
 func DeleteApp(id int) (err error) {
 	o := orm.NewOrm()
 	v := App{Id: id}
+	// ascertain id exists in the database
 	if err = o.Read(&v); err == nil {
 		var num int64
 		if num, err = o.Delete(&App{Id: id}); err == nil {
-			fmt.Println("Number of records deleted in database: ", num)
+			fmt.Println("Number of records deleted in database:", num)
 		}
 	}
 	return
