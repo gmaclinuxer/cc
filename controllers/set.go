2c44cab257ea1a2b082096bc52ecbf990f1c8e50
shwinpiocess <springwind_lee@163.com>
2016-03-22 23:41:39 +0800

    新增集群控制方法

--- controllers/set.go
+++ controllers/set.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"strconv"
 	"github.com/shwinpiocess/cc/models"
 )
@@ -50,7 +51,7 @@ func (this *SetController) NewSet() {
 	s.ChnName = this.GetString("ChnName")
 	s.Capacity, _ = this.GetInt("Capacity")
 	s.Description = this.GetString("Des")
-	s.OpenStatus, _ = this.GetInt("Openstatus")
+	s.OpenStatus = this.GetString("Openstatus")
 	s.Owner = this.userId
 	
 	if _, err := models.AddSet(s); err == nil {
@@ -59,6 +60,70 @@ func (this *SetController) NewSet() {
 	} else {
 		out["success"] = false
 		out["errInfo"] = "重复的集群名！"
+		this.jsonResult(out)
+	}
+}
+
+func (this *SetController) GetSetInfoById() {
+	out := make(map[string]interface{})
+	
+	if setID, err := this.GetInt("SetID"); err == nil {
+		if s, err := models.GetSetById(setID); err == nil {
+			out["success"] = true
+			out["set"] = s
+			this.jsonResult(out)
+		} else {
+			out["success"] = false
+			out["errInfo"] = "集群不存在！"
+			this.jsonResult(out)
+		}
+	} else {
+		out["success"] = false
+		out["errInfo"] = "请求参数不合法！"
+		this.jsonResult(out)
+	}
+}
+
+func (this *SetController) EditSet() {
+	out := make(map[string]interface{})
+	
+	s := new(models.Set)
+	s.ApplicationID, _ = this.GetInt("ApplicationID")
+	s.SetName = this.GetString("SetName")
+	s.EnviType, _ = this.GetInt("EnviType")
+	s.ServiceStatus, _ = this.GetInt("ServiceStatus")
+	s.ChnName = this.GetString("ChnName")
+	s.Capacity, _ = this.GetInt("Capacity")
+	s.Description = this.GetString("Des")
+	s.OpenStatus = this.GetString("Openstatus")
+	s.SetID, _ = this.GetInt("SetID")
+	
+	if err := models.UpdateSetById(s); err == nil {
+		out["success"] = true
+		this.jsonResult(out)
+	} else {
+		fmt.Println("err=", err)
+		out["success"] = false
+		out["errInfo"] = err
+		this.jsonResult(out)
+	}
+}
+
+func (this *SetController) DelSet() {
+	out := make(map[string]interface{})
+
+	if setID, err := this.GetInt("SetID"); err == nil {
+		if err := models.DeleteSet(setID); err == nil {
+			out["success"] = true
+			this.jsonResult(out)
+		} else {
+			out["success"] = false
+			out["errInfo"] = err
+			this.jsonResult(out)
+		}
+	} else {
+		out["success"] = false
+		out["errInfo"] = "请求参数不合法！"
 		this.jsonResult(out)
 	}
 }
